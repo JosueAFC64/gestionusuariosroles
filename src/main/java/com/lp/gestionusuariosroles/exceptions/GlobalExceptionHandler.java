3ddf557822012411a1eeda6739fc88374a085434
JosueAFC64 <josueadrianfranco@gmail.com>
2025-06-22 15:49:18 -0500

    Corrección del GlobalExceptionHandler
    - Las excepciones de tipo "Not Found" ahora devolverán un status "HttpStatus.NOT_FOUND" en lugar de un "Bad Request"

--- src/main/java/com/lp/gestionusuariosroles/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/lp/gestionusuariosroles/exceptions/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.lp.gestionusuariosroles.exceptions;
 
 import com.lp.gestionusuariosroles.error.ErrorResponse;
 import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -27,7 +28,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(UsernameNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleUsernameNotFoundException(UsernameNotFoundException e){
-        return ResponseEntity.badRequest().body(new ErrorResponse(404, "Not found", e.getMessage()));
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponse(404, "Not Found", e.getMessage()));
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
@@ -37,7 +38,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleEntityNotFoundException(EntityNotFoundException e){
-        return ResponseEntity.badRequest().body(new ErrorResponse(404, "Not found", e.getMessage()));
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponse(404, "Not Found", e.getMessage()));
     }
 
     @ExceptionHandler(BadCredentialsException.class)
